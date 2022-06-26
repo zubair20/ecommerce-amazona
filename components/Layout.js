349c9427d9afe7024c3a04e91d60dfb7db728ff7
= <=>
2022-06-26 18:37:41 +0500

    Base Layout added

---
+++ components/Layout.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Layout({title, children}) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='flex min-h-screen flex-col justify-between'>
+        <header>
+          <nav className='flex h-12 justify-between items-center px-4 shadow-md'>
+            <Link href='/'>
+              <a className='text-lg font-bold'>Amazona</a>
+            </Link>
+            <div>
+              <Link href='/cart'><a className='p-2'>Cart</a></Link>
+              <Link href='/login'><a className='p-2'>Login</a></Link>
+            </div>
+          </nav>
+        </header>
+        <main className='container m-auto mt-4 px-4'>
+          {children}
+        </main>
+        <footer className='flex h-10 justify-center items-center shadow-inner'>
+          <p>Copyright &copy; 2022 Amazona</p>
+        </footer>
+      </div>
+    </>
+  )
+}
