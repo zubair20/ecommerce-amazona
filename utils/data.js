b120e925ca01534fde00337f6ff2e6cbe9a6df87
= <=>
2022-06-26 19:13:32 +0500

    design product cards

---
+++ utils/data.js
@@ -0,0 +1,79 @@
+
+const data = {
+  products: [
+    {
+      name: 'Free Shirt',
+      slug: 'free-shirt',
+      category: 'Shirts',
+      image: '/images/shirt1.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      discription: 'A popular Shirt'
+    },
+    {
+      name: 'Free Shirt',
+      slug: 'free-shirt',
+      category: 'Shirts',
+      image: '/images/shirt2.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      discription: 'A popular Shirt'
+    },
+    {
+      name: 'Free Shirt',
+      slug: 'free-shirt',
+      category: 'Shirts',
+      image: '/images/shirt3.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      discription: 'A popular Shirt'
+    },
+    {
+      name: 'Free Shirt',
+      slug: 'free-shirt',
+      category: 'Shirts',
+      image: '/images/shirt1.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      discription: 'A popular Shirt'
+    },
+    {
+      name: 'Free Shirt',
+      slug: 'free-shirt',
+      category: 'Shirts',
+      image: '/images/shirt2.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      discription: 'A popular Shirt'
+    },
+    {
+      name: 'Free Shirt',
+      slug: 'free-shirt',
+      category: 'Shirts',
+      image: '/images/shirt3.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      discription: 'A popular Shirt'
+    }
+  ]
+}
+
+export default data;
